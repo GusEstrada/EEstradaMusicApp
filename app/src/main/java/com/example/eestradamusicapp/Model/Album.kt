e902811a723514ae46f204f048b571d9197e45fd
GusEstrada <gustavo.medina8124@gmail.com>
2025-10-18 18:38:55 -0600

    Modelo y servicio con retrofit

--- app/src/main/java/com/example/eestradamusicapp/Model/Album.kt
+++ app/src/main/java/com/example/eestradamusicapp/Model/Album.kt
@@ -1,1 +1,9 @@
-package com.example.eestradamusicapp.Model
+package com.example.eestradamusicapp.model
+
+data class Album(
+    val id: String,
+    val title: String,
+    val artist: String,
+    val description: String,
+    val image: String
+)
---
+++ app/src/main/java/com/example/eestradamusicapp/Services/AlbumService.kt
@@ -0,0 +1,15 @@
+package com.example.eestradamusicapp.services
+
+import com.example.eestradamusicapp.model.Album
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface AlbumService {
+    @GET("albums")
+    suspend fun getAllAlbums(): List<Album>
+
+
+    @GET("albums/{id}")
+    suspend fun getAlbumById(@Path("id") id: String): Album
+}
